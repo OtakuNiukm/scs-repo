8124a3f575e037da7f85dfdf82d324e8d05bd6f2
Otaku Niukm <166994459+OtakuNiukm@users.noreply.github.com>
2024-10-12 17:10:37 +0800

    网关依赖和配置

---
+++ cloud-iaas/gateway-server/src/main/java/com/otaku/GatewayServerApplication.java
@@ -0,0 +1,16 @@
+package com.otaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 网关服务启动类
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+}
